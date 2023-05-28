cff2ee95fea27d0af2ed7fea8eb44bbb244f9e3a
QuanlingZhao <quzhao@ucsd.edu>
2023-05-28 00:15:12 -0700

    Update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lab 8 - Starter
+## Name: Quanling Zhao
+1. We should run automated tests within a Github action that runs whenever code is pushed, this allows as to catch bugs and error early before going too deep into development and allow fast iteration in Agile.
+2. No, unit testing is more suitable for testing a specific function. E2E test is more useful will test and evaluate the entire progeam holistically.
+3. I would not use unit test for message function as it is a complex feature that involves many parts (senting, receving, etc...).
+4. I would use unit test for max_length function as it is a simply function that can be texted extensively without consider its interactions with other components.
+5. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,98 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//isPhoneNumber
+test('isPhoneNumber-True-1', () => {
+  expect(functions.isPhoneNumber("626-7846765")).toBe(true);
+});
+
+test('isPhoneNumber-True-2', () => {
+  expect(functions.isPhoneNumber("256-7965476")).toBe(true);
+});
+
+test('isPhoneNumber-False-1', () => {
+  expect(functions.isPhoneNumber("32redadf")).toBe(false);
+});
+
+test('isPhoneNumber-False-2', () => {
+  expect(functions.isPhoneNumber("wqef34w3edr")).toBe(false);
+});
+
+
+
+//isEmail
+test('isEmail-True-1', () => {
+  expect(functions.isEmail("1wqed@gamil.com")).toBe(true);
+});
+
+test('isEmail-True-2', () => {
+  expect(functions.isEmail("13e3efwdd@yahoo.com")).toBe(true);
+});
+
+test('isEmail-False-1', () => {
+  expect(functions.isEmail("2edf23edfq")).toBe(false);
+});
+
+test('isEmail-False-2', () => {
+  expect(functions.isEmail("234-545-7333")).toBe(false);
+});
+
+
+
+//isStrongPassword
+test('isStrongPassword-True-1', () => {
+  expect(functions.isStrongPassword("ATY8374672")).toBe(true);
+});
+
+test('isStrongPassword-True-2', () => {
+  expect(functions.isStrongPassword("H4338TY472")).toBe(true);
+});
+
+test('isStrongPassword-False-1', () => {
+  expect(functions.isStrongPassword("NHJHAHADFGHFHAFIUHAIFUHDFBSHF")).toBe(false);
+});
+
+test('isStrongPassword-False-2', () => {
+  expect(functions.isStrongPassword("134234234234")).toBe(false);
+});
+
+
+
+//isDate
+test('isDate-True-1', () => {
+  expect(functions.isDate("05/02/1975")).toBe(true);
+});
+
+test('isDate-True-2', () => {
+  expect(functions.isDate("14/11/2023")).toBe(true);
+});
+
+test('isDate-False-1', () => {
+  expect(functions.isDate("626-9278635")).toBe(false);
+});
+
+test('isDate-False-2', () => {
+  expect(functions.isDate("qd232@ucsd.edu")).toBe(false);
+});
+
+
+
+//isHexColor
+test('isHexColor-True-1', () => {
+  expect(functions.isHexColor("d1b598")).toBe(true);
+});
+
+test('isHexColor-True-2', () => {
+  expect(functions.isHexColor("ffffff")).toBe(true);
+});
+
+test('isHexColor-False-1', () => {
+  expect(functions.isHexColor("rththf")).toBe(false);
+});
+
+test('isHexColor-False-2', () => {
+  expect(functions.isHexColor("tyuuiu")).toBe(false);
+});
+
+
+
